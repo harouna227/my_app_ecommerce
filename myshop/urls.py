9c0f9bc5c1df03a629fbea3cbff93e5799ad317d
Harouna227 <issifiharouna07@gmail.com>
2024-02-10 20:37:50 +0100

    my last commit

---
+++ myshop/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.shop, name='shop'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
+]
--- myshop/views.py
+++ myshop/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def shop(request):
+    context = {}
+    return render(request, 'myshop/shop.html')
+
+
+def cart(request):
+    context = {}
+    return render(request, 'myshop/cart.html')
+
+
+def checkout(request):
+    context = {}
+    return render(request, 'myshop/checkout.html')
