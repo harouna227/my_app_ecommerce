59d02baabaf0b6aa44c476015d2d42c4fd3f12c3
harouna227 <issifiharouna07@gmail.com>
2024-06-06 00:12:24 +0300

    my commit

---
+++ myshop/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.shop, name='shop'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
+
+    path('update-item/', views.update_item, name='update-item'),
+]
